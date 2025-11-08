ee8bdb521fdae2206243021378cc35ae71307701
Srj0210 <sleepyspider6166@gmail.com>
2025-11-08 13:09:08 +0530

    Delete tools directory

--- tools/image_tools.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-def compress_image(file, output_path, quality=40):
-    img = Image.open(file)
-    img.save(output_path, "JPEG", optimize=True, quality=quality)
--- tools/text_tools.py
+++
@@ -1,9 +0,0 @@
-def convert_case(text, mode):
-    if mode == "upper":
-        return text.upper()
-    elif mode == "lower":
-        return text.lower()
-    elif mode == "title":
-        return text.title()
-    else:
-        return text
